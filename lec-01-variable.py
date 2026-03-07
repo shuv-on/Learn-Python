961557cec8fc7784ba18dbaa3c8fbe8c022d457f
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2026-03-07 11:48:14 +0600

    Level 01

---
+++ lec-01-variable.py
@@ -0,0 +1,5 @@
+file_name = "result.pdf"
+file_size = "4.5 MB"
+total_page = 12
+
+print(file_name, file_size, total_page)
---
+++ lec-02-user-input.py
@@ -0,0 +1,4 @@
+user_name = input("Enter Your Name: ")
+pdf_number = int(input("Enter total number of PDF: "))
+
+print(f"Hello {user_name}, Your {pdf_number} pdf is ready to merge by TermiPDF")
