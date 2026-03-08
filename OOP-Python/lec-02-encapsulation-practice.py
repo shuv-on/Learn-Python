45f7948df1509d57369d23c15143f023297a9525
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2026-03-09 02:05:54 +0600

    Encapsulation Practice

--- OOP-Python/lec-02-encapsulation-practice.py
+++ OOP-Python/lec-02-encapsulation-practice.py
@@ -4,8 +4,8 @@ class UserWallet:
         self.__balance = balance
     def add_money(self, amount):
         if amount > 0:
-            amount+=self.__balance
-            print(f"Money added. New balance is {amount}")
+            self.__balance+=amount
+            print(f"Money added. New balance is {self.__balance}")
         else:
             print("Invalid amount")
 user_wallet = UserWallet("Shuvon", 500) 
